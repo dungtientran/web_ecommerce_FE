bb6e816496d55a04924db23fb3916f8f49031cf6
tran dung <taquests@gmail.com>
2023-03-08 14:06:46 +0700

    tr

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-
+const sever_URL = 'https://heliosweb.onrender.com';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL
-})
+    // baseURL: process.env.REACT_APP_SERVER_URL
+    baseURL : sever_URL
+}) 
 
 
 export default instance
